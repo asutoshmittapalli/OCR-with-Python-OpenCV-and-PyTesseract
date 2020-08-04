0e430158d8694683b2b86145307717aed0d2008a
asutoshmittapalli <asutoshmittapalli@gmail.com>
2020-08-04 07:15:37 +0530

    box_img.py

---
+++ box_img.py
@@ -0,0 +1,8 @@
+n_boxes = len(d['text'])
+for i in range(n_boxes):
+    if int(d['conf'][i]) > 60:
+        (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
